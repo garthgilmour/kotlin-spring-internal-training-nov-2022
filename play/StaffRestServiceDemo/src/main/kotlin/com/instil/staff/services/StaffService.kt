a8e96ebee55e4a1e7408ad8a6045fd576147fdb9
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-08 14:55:32 +0000

    Staff service iteration 6 - Add endpoints for DELETE and PUT

--- play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/services/StaffService.kt
+++ play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/services/StaffService.kt
@@ -4,17 +4,14 @@ import com.instil.staff.model.Department
 import com.instil.staff.model.Department.*
 import com.instil.staff.model.Employee
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.notFound
-import org.springframework.http.ResponseEntity.ok
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity.*
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/staff")
-class StaffService(private val staff: List<Employee>) {
+class StaffService(private val staff: MutableList<Employee>) {
 
     @GetMapping(produces = [APPLICATION_JSON_VALUE])
     fun allStaff() = if (staff.isEmpty()) {
@@ -44,6 +41,36 @@ class StaffService(private val staff: List<Employee>) {
     fun staffByName(@PathVariable name: String) = findStaff {
         it.name.equals(name, true)
     }
+
+    @DeleteMapping(
+        value = ["/byName/{name}"],
+        produces = [TEXT_PLAIN_VALUE]
+    )
+    fun deleteStaffByName(@PathVariable name: String): ResponseEntity<String> {
+        val result = staff.removeIf {
+            it.name.equals(name, true)
+        }
+
+        return if (result) ok("Deleted $name") else notFound().build()
+    }
+
+    @PutMapping(
+        consumes = [APPLICATION_JSON_VALUE], //ContentType header
+        produces = [TEXT_PLAIN_VALUE]        //Accept header
+    )
+    fun addStaff(@RequestBody newEmployee: Employee): ResponseEntity<String> {
+        fun build404() = badRequest().body("${newEmployee.name} already exists!")
+        fun build200() = ok("${newEmployee.name} created")
+
+        return if(alreadyExists(newEmployee)) {
+            build404()
+        } else {
+            staff.add(newEmployee)
+            build200()
+        }
+    }
+
+    private fun alreadyExists(newEmployee: Employee) = staff.any { it.name.equals(newEmployee.name, true) }
 
     private fun findStaff(predicate: (Employee) -> Boolean): ResponseEntity<Employee> {
         val result = staff.firstOrNull(predicate)
