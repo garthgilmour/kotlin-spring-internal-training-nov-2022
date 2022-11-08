4b498db48f94cc8543dbb58887164520f7a3eec1
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-08 13:50:58 +0000

    Staff service iteration 3 - Show using Application Context

--- play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/StaffApplication.kt
+++ play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/StaffApplication.kt
@@ -2,9 +2,20 @@ package com.instil.staff
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.AbstractApplicationContext
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class StaffApplication
+class StaffApplication(val context: AbstractApplicationContext) {
+	@PostConstruct
+	fun showBeans() {
+		println("Registered beans:")
+		context.beanDefinitionNames.forEach { name ->
+			println("Bean named '$name'")
+		}
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<StaffApplication>(*args)
