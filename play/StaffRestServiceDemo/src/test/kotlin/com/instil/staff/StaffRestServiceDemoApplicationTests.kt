a2544646227b17b4dfd4b04d2f42e598a119fa0f
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-08 11:48:58 +0000

    Staff service iteration 0 - Skeleton Project

---
+++ play/StaffRestServiceDemo/src/test/kotlin/com/instil/staff/StaffRestServiceDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.instil.staff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StaffRestServiceDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
