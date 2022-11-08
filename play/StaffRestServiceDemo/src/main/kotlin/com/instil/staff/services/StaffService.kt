806e1a394887161b78b58f47989851f82253b246
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-08 14:09:11 +0000

    Staff service iteration 4 - Add error handling

--- play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/services/StaffService.kt
+++ play/StaffRestServiceDemo/src/main/kotlin/com/instil/staff/services/StaffService.kt
@@ -4,6 +4,9 @@ import com.instil.staff.model.Department
 import com.instil.staff.model.Department.*
 import com.instil.staff.model.Employee
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.notFound
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,29 +17,54 @@ import org.springframework.web.bind.annotation.RestController
 class StaffService(private val staff: List<Employee>) {
 
     @GetMapping(produces=[APPLICATION_JSON_VALUE])
-    fun allStaff() = staff
+    fun allStaff() = if(staff.isEmpty()) {
+        notFound().build()
+    } else {
+        ok(staff)
+    }
 
     @GetMapping(
         value=["/byAge/{age}"],
         produces=[APPLICATION_JSON_VALUE]
     )
-    fun staffByAge(@PathVariable age: Int) = staff.filter {
-        it.age == age
+    fun staffByAge(@PathVariable age: Int): ResponseEntity<List<Employee>> {
+        val results = staff.filter {
+            it.age == age
+        }
+        return if(results.isEmpty()) {
+            notFound().build()
+        } else {
+            ok(staff)
+        }
     }
 
     @GetMapping(
         value=["/byDept/{department}"],
         produces=[APPLICATION_JSON_VALUE]
     )
-    fun staffByDept(@PathVariable department: Department) = staff.filter {
-        it.department == department
+    fun staffByDept(@PathVariable department: Department): ResponseEntity<List<Employee>> {
+        val results = staff.filter {
+            it.department == department
+        }
+        return if(results.isEmpty()) {
+            notFound().build()
+        } else {
+            ok(staff)
+        }
     }
-
     @GetMapping(
         value=["/byName/{name}"],
         produces=[APPLICATION_JSON_VALUE]
     )
-    fun staffByName(@PathVariable name: String) = staff.firstOrNull {
-        it.name.equals(name, true)
+    fun staffByName(@PathVariable name: String): ResponseEntity<Employee> {
+        val result = staff.firstOrNull {
+            it.name.equals(name, true)
+        }
+
+        return if(result == null) {
+            notFound().build()
+        } else {
+            ok(result)
+        }
     }
 }
